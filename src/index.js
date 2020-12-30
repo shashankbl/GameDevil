875f1319ebfa27b0fcc1d5ab51edf65e996a2097
Shashank BL <shashankbl.89@gmail.com>
2020-12-29 20:20:36 -0700

    working paddle

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Paddle from "./paddle.js";
+import InputHandler from "./input.js";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -10,4 +11,21 @@ ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
+new InputHandler(paddle);
+
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
--- src/paddle.js
+++ src/paddle.js
@@ -1,16 +1,43 @@
 export default class Paddle {
   constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+
     this.width = 150;
     this.height = 20;
 
+    this.maxSpeed = 8;
+    this.speed = 0;
+
     this.position = {
       x: gameWidth / 2 - this.width / 2,
-      y: gameHeight - this.height - 10
+      y: gameHeight - this.height - 5
     };
+  }
+
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
+
+  moveRight() {
+    this.speed = +this.maxSpeed;
+  }
+
+  stop() {
+    this.speed = 0;
   }
 
   draw(ctx) {
     ctx.fillStyle = "#f00";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    if (!deltaTime) return;
+
+    this.position.x += this.speed;
+
+    if (this.position.x < 5) this.position.x = 5;
+    if (this.position.x + this.width > this.gameWidth - 5)
+      this.position.x = this.gameWidth - this.width - 5;
   }
 }
